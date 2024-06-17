d6d4ed8e2cbfdfceec5a44e1761b42be9dc85071
williwilliams3 <williwilliams3@hotmail.com>
2024-06-17 16:12:53 +0300

    Fix dependencies

--- ppe/__init__.py
+++ ppe/__init__.py
@@ -1,7 +1,4 @@
-from dirichlet import Dirichlet
-from priorpredictiveelicitation import PPEProbabilities
-from gradient_optimization import optimize_ppe
-from bayesian_optimization import Bayesian_Optimization
+from .dirichlet import Dirichlet
+from .priorpredictiveelicitation import PPEProbabilities
 
-
-__all__ = ["Dirichlet", "PPEProbabilities", "optimize_ppe", "Bayesian_Optimization"]
+__all__ = ["Dirichlet", "PPEProbabilities"]
