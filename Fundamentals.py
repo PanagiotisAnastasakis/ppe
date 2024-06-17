135dd430423352728f07086d478fbafe906aa164
PanagiotisAnastasakis <panosanast2001@gmail.com>
2024-06-17 12:11:03 +0300

    Update Fundamentals.py

--- Fundamentals.py
+++ Fundamentals.py
@@ -507,7 +507,23 @@ class optimize_ppe(Dirichlet): ### closed form is assumed!!!
 
 
 
+## Class to perform Bayesian Optimization to find the optimal hyperparameters for the priors
+
+
+
 class Bayesian_Optimization(Dirichlet, PPEProbabilities):
+    
+    
+    '''
+    Inputs:
+    
+    - "pymc_sampling_func" -> The PyMC function that defines our probabilistic model for the target (i.e. the prior predictive distribution)
+    - "J" -> The number of covariate sets that the expert gives probabilistic judgements for
+    - "alpha" -> A value for the hyperparameter alpha of the dirichlet likelihood. If None, it is optimized along with all other hyperparameters
+    - "target_type" -> parameter for whether the target is continuous or discrete. Options: ["continuous", "discrete"]
+    - "target_samples" -> the number of samples that we draw from the prior predictive distribution
+    
+    '''
     
     def __init__(self, pymc_sampling_func, J, alpha, target_type, target_samples = 500):
         Dirichlet.__init__(self, alpha, J)
@@ -515,6 +531,18 @@ class Bayesian_Optimization(Dirichlet, PPEProbabilities):
         self.pymc_sampling_func = pymc_sampling_func
         self.target_samples = target_samples
         
+        
+        
+    ## Function to get the model probabilities based on the acquired samples from the prior predictive distribution.
+    
+    '''
+    Inputs:
+    
+    - "lam" -> the hyperparameters
+    - "partitions" -> the partitioning for the target space for each covariate set. Can be either a list (J=1) or a list of lists for each covariate set (J>1)
+
+    '''
+            
     def get_model_probs(self, lam, partitions):
         
         idata = self.pymc_sampling_func(lam, self.target_samples)
@@ -528,8 +556,20 @@ class Bayesian_Optimization(Dirichlet, PPEProbabilities):
         model_probs = self.ppd_probs(samples = samples, partitions = partitions)
         
         return model_probs
+    
+    
         
-            
+    ## Function to get the negative of dirichlet log likelihood.
+    
+    '''
+    Inputs:
+    
+    - "lam" -> the hyperparameters
+    - "partitions" -> the partitioning for the target space for each covariate set. Can be either a list/array (J=1) or a list/array of lists/arrays for each covariate set (J>1)
+    - "expert_probabilities" -> the expert's probabilistic judgements for each covariate set. Can be either a list (J=1) or a list of lists for each covariate set (J>1)
+
+    '''
+                
     def dirichlet_neg_llik(self, lam, partitions, expert_probs):
         
         model_probs = self.get_model_probs(lam, partitions)
@@ -545,6 +585,30 @@ class Bayesian_Optimization(Dirichlet, PPEProbabilities):
             self.alpha = None
             
         return float(-dir_llik)
+    
+    
+    
+    ## Function to include hyperpriors for the hyperparameters.
+    
+    ## The idea is that if the user can give an input regarding some of the prior hyperparameters, then we can take that into consideration by adding 
+    ## hyperpriors in the loss function which encourage values close to that input. Additionally, the user could give a confidence value between 0 and 1
+    ## regarding this input, which will act as weight for the hyperprior.
+    
+    ## If hyperpriors are employed, the loss will be: -dir_log_likelihood - log_hyperpriors * weights * J
+    
+    ## The hyperprior is a truncated gaussian with mean = user input, bounds that are centered around the mean and are of the same width as the ones used for the optimization
+    ## and standard deviation is half the length of the bounds.
+    
+    '''
+    Inputs:
+    
+    - "lam" -> the hyperparameters
+    - "param_bounds" -> the bounds that define the search space for each hyperparameter. List of lists, each corresponding to one hyperparameter and consisting of a lower and upper bound
+    - "param_expected_vals" -> the expert's estimates for the hyperparameters. a list of numbers that accepts also None if no such input can be given.
+    - "param_weights" -> the level of confidence for these expected value (a list of values)
+
+    '''
+    
     
     def hyperprior_llik(self, lam, param_bounds, param_expected_vals, param_weights):
                 
@@ -577,6 +641,23 @@ class Bayesian_Optimization(Dirichlet, PPEProbabilities):
     
     
     
+    
+    ## Function to optimize the hyperparameters using the function "optimize" from "Ax".
+    
+    
+    '''
+    Inputs:
+    
+    - "param_names" -> the names for the hyperparameters in a list
+    - "param_bounds" -> the bounds that define the search space for each hyperparameter. List of lists, each corresponding to one hyperparameter and consisting of a lower and upper bound
+    - "param_expected_vals" -> the expert's estimates for the hyperparameters. a list of numbers that accepts also None if no such input can be given.
+    - "param_weights" -> the level of confidence for these expected value (a list of values)
+    - "partitions" -> the partitioning for the target space for each covariate set. Can be either a list/array (J=1) or a list/array of lists/arrays for each covariate set (J>1)
+    - "expert_probabilities" -> the expert's probabilistic judgements for each covariate set. Can be either a list (J=1) or a list of lists for each covariate set (J>1)
+
+    '''
+    
+    
     def optimize_hyperparams(self,
                              param_names: list,
                              param_bounds: list,
@@ -599,6 +680,19 @@ class Bayesian_Optimization(Dirichlet, PPEProbabilities):
             )
         
         return best_lam
+    
+    
+    
+    ## Function to evaluate a set of hyperparameters by computing alpha
+    
+    '''
+    Inputs:
+    
+    - "lam" -> the hyperparameters
+    - "partitions" -> the partitioning for the target space for each covariate set. Can be either a list/array (J=1) or a list/array of lists/arrays for each covariate set (J>1)
+    - "expert_probabilities" -> the expert's probabilistic judgements for each covariate set. Can be either a list (J=1) or a list of lists for each covariate set (J>1)
+
+    '''
     
             
     def eval_function(self, lam, partitions, expert_probs):
