9f9c336d466dfd1a98fec9b5f308be4bb15c3a33
williwilliams3 <williwilliams3@hotmail.com>
2024-06-21 11:53:22 +0300

    Create stochastic opt testing

--- ppe/stochastic_optimization.py
+++ ppe/stochastic_optimization.py
@@ -2,84 +2,65 @@ import jax
 import jax.numpy as jnp
 import jax.random as jr
 import jax.scipy.stats as jss
-from dirichlet import dirichlet_log_likelihood, alpha_mle_
+from .dirichlet import dirichlet_log_likelihood, alpha_mle_
 
 
-def get_gaussian_probs(partitions, lam):
-
-    mu_1 = lam[0]
-    sigma = lam[1]
-    sigma_1 = lam[2]
-
-    p1 = jss.norm.cdf(
-        (partitions[:, 1] - mu_1) / jnp.sqrt(sigma**2 + sigma_1**2)
-    ) - jss.norm.cdf((partitions[:, 0] - mu_1) / jnp.sqrt(sigma**2 + sigma_1**2))
-
-    return p1
-
-
-@jax.jit
-def nonstochastic_derivative(alpha, probs, expert_probs):
-    # Compute the gradient of the dirichlet likelihood wrt the probabilities
-
-    if alpha is None:
-        # If alpha is not provided, compute the MLE
-        def likelihood_fn(probs):
-            # TODO: Bug here
-            alpha = alpha_mle_(probs, expert_probs)
-            return dirichlet_log_likelihood(alpha, probs, expert_probs)
-
-    else:
-        likelihood_fn = lambda probs: dirichlet_log_likelihood(
-            alpha, probs, expert_probs
-        )
-
-    grad_fn = jax.grad(likelihood_fn)
-    likelihood_gradient = grad_fn(probs)
-    return likelihood_gradient
+def replace_first_occurrence(lst, old_value, new_value):
+    # Iterate through the list
+    for i in range(len(lst)):
+        # Check if the current element is the one to replace
+        if lst[i] == old_value:
+            # Replace it with the new value
+            lst[i] = new_value
+            # Exit the loop after the first replacement
+            break
+    return lst
 
 
-@jax.jit
-def stochastic_derivative(lambd, partition):
-    # Compute stochastic gradient of each prior predictive probability wrt the hyperparameters
-    a, b = partition
-    pivot_sample = sampler_fn(rng_key, (num_samples,))
+def set_derivative_fn(
+    rng_key,
+    num_samples,
+    sampler_fn,
+    cdf_fn,
+    pivot_fn,
+    total_model_probs,
+    total_expert_probs,
+):
 
-    def function_to_optimize(lambd, a, b):
-        theta = pivot_fn(lambd, pivot_sample)
-        return (cdf_fn(theta, b, lambd) - cdf_fn(theta, a, lambd)).mean()
+    @jax.jit
+    def nonstochastic_derivative(alpha, probs, expert_probs):
+        # Compute the gradient of the dirichlet likelihood wrt the probabilities
 
-    stochastic_gradient = jax.grad(function_to_optimize)
-    return stochastic_gradient(lambd, a, b)
+        if alpha is None:
+            # If alpha is not provided, compute the MLE
+            def likelihood_fn(probs):
+                # Make the computation of alpha depend on the current probs
+                total_model_probs = replace_first_occurrence(
+                    total_model_probs, probs, probs
+                )
+                alpha = alpha_mle_(total_model_probs, total_expert_probs)
+                return dirichlet_log_likelihood(alpha, probs, expert_probs)
 
+        else:
+            likelihood_fn = lambda probs: dirichlet_log_likelihood(
+                alpha, probs, expert_probs
+            )
 
-if __name__ == "__main__":
-    partitions = jnp.array([[-1000, -2], [-2, 3], [3, 1000]])
-    expert_probs = jnp.array([0.2, 0.7, 0.1])
+        grad_fn = jax.grad(likelihood_fn)
+        likelihood_gradient = grad_fn(probs)
+        return likelihood_gradient
 
-    # Try simple Gaussian example
-    alpha = 1.0
-    lambd_0 = jnp.ones(3)
-    rng_key = jr.key(0)
-    num_samples = 1_000_000
-    sampler_fn = jr.normal
-    cdf_fn = lambda theta, a, lambd: jss.norm.cdf(a, loc=theta, scale=lambd[-1])
-    pivot_fn = lambda lambd, z: lambd[0] + lambd[1] * z
-    # In this case we can obtain probs in closed form, but in general we would need stochastic estimates
-    probs = get_gaussian_probs(partitions, lambd_0)
-    derivative_1 = nonstochastic_derivative(alpha, probs, expert_probs)
-    vmap_stochastic_derivative = jax.vmap(stochastic_derivative, in_axes=(None, 0))
-    derivative_2 = vmap_stochastic_derivative(lambd_0, partitions)
-    derivative = jnp.dot(derivative_2.T, derivative_1)
-    print("likelihood derivative", derivative_1)
-    print("stochastic derivative", derivative_2)
-    print("stochastic gradient", derivative)
+    @jax.jit
+    def stochastic_derivative(lambd, partition):
+        # Compute stochastic gradient of each prior predictive probability wrt the hyperparameters
+        a, b = partition
+        pivot_sample = sampler_fn(rng_key, (num_samples,))
 
-    def test_fn(lambd):
-        probs = get_gaussian_probs(partitions, lambd)
-        return dirichlet_log_likelihood(alpha, probs, expert_probs)
+        def function_to_optimize(lambd, a, b):
+            theta = pivot_fn(lambd, pivot_sample)
+            return (cdf_fn(theta, b, lambd) - cdf_fn(theta, a, lambd)).mean()
 
-    test_value = jax.grad(test_fn)(lambd_0)
-    print("Non stochastic gradient", test_value)
-    assert jnp.allclose(test_value, derivative, atol=1e-1)
-    print("Test passed")
+        stochastic_gradient_fn = jax.value_and_grad(function_to_optimize)
+        return stochastic_gradient_fn(lambd, a, b)
+
+    return nonstochastic_derivative, stochastic_derivative
--- tests/stochastic_gradient.py
+++ tests/stochastic_gradient.py
@@ -1,1 +1,69 @@
+import os
+import sys
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+
+
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import jax.scipy.stats as jss
+from ppe.dirichlet import dirichlet_log_likelihood
+from ppe.stochastic_optimization import set_derivative_fn
+
+
+def get_gaussian_probs(partitions, lam):
+    #  Vectorized Gaussian CDF of simple Gaussian model
+    mu_1 = lam[0]
+    sigma = lam[1]
+    sigma_1 = lam[2]
+
+    p1 = jss.norm.cdf(
+        (partitions[:, 1] - mu_1) / jnp.sqrt(sigma**2 + sigma_1**2)
+    ) - jss.norm.cdf((partitions[:, 0] - mu_1) / jnp.sqrt(sigma**2 + sigma_1**2))
+
+    return p1
+
+
 # TODO: Add test here
+if __name__ == "__main__":
+    partitions = jnp.array([[-1000, -2], [-2, 3], [3, 1000]])
+    expert_probs = jnp.array([0.2, 0.7, 0.1])
+
+    # Try simple Gaussian example
+    alpha = 1.0
+    lambd_0 = jnp.ones(3)
+    rng_key = jr.key(0)
+    num_samples = 1_000_000
+    sampler_fn = jr.normal
+    cdf_fn = lambda theta, a, lambd: jss.norm.cdf(a, loc=theta, scale=lambd[-1])
+    pivot_fn = lambda lambd, z: lambd[0] + lambd[1] * z
+    # In this case we can obtain probs in closed form, but in general we would need stochastic estimates
+    probs = get_gaussian_probs(partitions, lambd_0)
+    total_model_probs = [probs]
+    total_expert_probs = [expert_probs]
+    nonstochastic_derivative, stochastic_derivative = set_derivative_fn(
+        rng_key,
+        num_samples,
+        sampler_fn,
+        cdf_fn,
+        pivot_fn,
+        total_model_probs,
+        total_expert_probs,
+    )
+    derivative_1 = nonstochastic_derivative(alpha, probs, expert_probs)
+    vmap_stochastic_derivative = jax.vmap(stochastic_derivative, in_axes=(None, 0))
+    _, derivative_2 = vmap_stochastic_derivative(lambd_0, partitions)
+    derivative = jnp.dot(derivative_2.T, derivative_1)
+
+    def test_fn(lambd):
+        probs = get_gaussian_probs(partitions, lambd)
+        return dirichlet_log_likelihood(alpha, probs, expert_probs)
+
+    test_value = jax.grad(test_fn)(lambd_0)
+    print("stochastic gradient", derivative)
+    print("Non stochastic gradient", test_value)
+    assert jnp.allclose(test_value, derivative, atol=1e-1)
+    print("Test passed")
